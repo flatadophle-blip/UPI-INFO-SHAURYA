097b4752ef455ebd4cfab01d5337ae49e829fc17
flatadophle-blip <flatadophle@gmail.com>
2025-12-29 00:29:15 +0530

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <pre style={{
+      fontFamily: "monospace",
+      fontSize: "18px",
+      padding: "40px"
+    }}>
+{`Dev : evil Spy
+Created : cause tobi ne bola`}
+    </pre>
+  );
+}
