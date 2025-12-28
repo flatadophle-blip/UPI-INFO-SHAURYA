3809391f11a06901f66928ebf8eeeed354b5913d
flatadophle-blip <flatadophle@gmail.com>
2025-12-29 00:28:49 +0530

    Delete api/index.js

--- api/index.js
+++
@@ -1,7 +0,0 @@
-export default function handler(req, res) {
-  res.setHeader("Content-Type", "text/plain; charset=utf-8");
-  res.status(200).send(
-`Dev : evil Spy
-Created : cause tobi ne bola`
-  );
-}
